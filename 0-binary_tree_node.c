6f4c41027a000303f9de58a493f6e29355ec8d44
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-04-01 21:20:45 +0100

    fixed number of lines in a function

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -16,43 +16,30 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		binary_tree_t *head = malloc(sizeof(binary_tree_t));
 
 		if (!head)
-		{
 			return (NULL);
-		}
 		head->parent = NULL;
 		head->left = NULL;
 		head->right = NULL;
 		head->n = value;
-
 		return (head);
 	}
 	else
 	{
-		/*binary_tree_t *current;
-		binary_tree_t *traverse;*/
 		binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
 		if (!new_node)
-		{
 			return (NULL);
-		}
 		new_node->parent = parent;
 		new_node->n = value;
 		new_node->left = NULL;
 		new_node->right = NULL;
-
 		if (value < parent->n)
-		{
 			parent->left = new_node;
-		}
 		else
 		{
 			if (value > parent->n)
-			{
 				parent->right = new_node;
-			}
 		}
 		return (new_node);
 	}
-
 }
