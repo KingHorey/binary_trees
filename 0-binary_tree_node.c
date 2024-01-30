c73a7c36f49e37109e8864f5b618637f0e2cce40
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-01-30 12:46:49 +0100

    redifine final checks before node insertion

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,6 +13,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
+	if (new_node == NULL)
+		return (NULL);
+
 	if (parent == NULL)
 	{	new_node->parent = NULL;
 		new_node->n = value;
@@ -27,15 +30,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		new_node->left = NULL;
 		new_node->right = NULL;
 		if (parent->n > value)
-		{
 			parent->left = new_node;
-			new_node->left = NULL;
-		}
 		else
-		{
 			parent->right = new_node;
-			new_node->right = NULL;
-		}
 	}
 	return (new_node);
 }
