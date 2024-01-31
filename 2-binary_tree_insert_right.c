c935dbf41b51a39a6ecfc1932d7c877126b77595
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-01-31 11:58:13 +0100

    change logic to comply with c89 standard

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,10 +1,12 @@
-clude "binary_trees.h"
+#include "binary_trees.h"
+
 /**
  * binary_tree_insert_right - inserts a node as the left-child of another node
  * @parent: pointer to the node to insert the left-child in
  * @value: value to store in the new node
  * Return: pointer to the created node, or NULL on failure or if parent is NULL
  */
+
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
@@ -14,34 +16,27 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
-	new_node->n = value;
-	if (value >= parent->n)
+	if (value)
 	{
-		if (parent->right != NULL)
-		{
-			if (value <= parent->right->n)
-			{
-				new_node->right = parent->right;
-				new_node->left = NULL;
-				new_node->parent = parent->right->parent;
-				parent->right->parent = new_node;
-				parent->right = new_node;
-			}
-			else
-			{
-				parent->right = new_node;
-				new_node->left = NULL;
-				new_node->parent = parent;
-				new_node->right = NULL;
-			}
-		}
-		else
-		{
-			parent->right = new_node;
-			new_node->parent = parent;
-			new_node->left = NULL;
-			new_node->right = NULL;
-		}
+		new_node->n = value;
+		new_node->parent = parent;
+		new_node->left = NULL;
+	}
+	else
+		return (NULL);
+	if (parent->right)
+	{
+		new_node->right = parent->right;
+		new_node->left = NULL;
+		parent->right->parent = new_node;
+		parent->right = new_node;
+	}
+	else
+	{
+		new_node->right = NULL;
+		parent->right = new_node;
 	}
 	return (new_node);
+
 }
+
