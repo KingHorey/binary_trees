7a13e558fdac918491f5cf9ebead954bfb655b91
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-02-01 06:48:23 +0100

    use recursion to get leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,8 +13,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (tree->left && tree->right)
-		return (2);
+	if (tree->right != NULL || tree->left != NULL)
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 	return (1);
 }
 
