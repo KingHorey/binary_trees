6c23a04fae48e01c25e6f0068a3c26b154c12cbd
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-02-01 06:38:48 +0100

    get number of leafs in a node

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -15,10 +15,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		return (0);
 	if (tree->left && tree->right)
 		return (2);
-	if (tree->left && tree->right == NULL)
-		return (1);
-	if (tree->left == NULL && tree->right)
-		return (1);
 	return (1);
 }
 
